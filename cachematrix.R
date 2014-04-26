27ee18ff3cec54e133d2b29c617ba32b43e4c34d
mavigno <ricardo@mavigno.com>
2014-04-26 14:12:41 -0300

    First commit after the fork
    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions are used to solve the inverse matrix and put it in a cache
+## allowing to reuse the cached matrix instead of calculating again
 
-## Write a short comment describing this function
+## makeCacheMatrix: creates a special matrix with caching abilities
+##                  it receives a normal matrix as an argument
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## initialize "m" 
+        m <- NULL
+        ## internal function to setup caching matrix functions
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        ## define function to get cached matrix
+        get <- function() x
+        ## define function to set the matrix inverse
+        setinverse <- function(solve) m <<- solve
+        ## define function to get the inverse of the matrix
+        getinverse <- function() m
+        ## return list to with special matrix functions
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function to solve the inverse of a matrix or return it from cache if it was
+## alredy solved
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## try to get the inverse from the cache
+        m <- x$getinverse()
+        ## if the matrix is in the cache, get the data and return it
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## if the matrix isn't in the cache, get the data to solve it
+        data <- x$get()
+        ## try to solve the inverse of the matrix, if its insolvable return an
+        ## error
+        m <- try(solve(data),silent=TRUE)
+        if ("try-error" %in% class(m)) {
+                stop("Matrix is not invertible")
+        }
+        ## store the inverse of the matrix in the cache
+        x$setinverse(m)
+        ## return the inverse of the matrix
+        m
 }
